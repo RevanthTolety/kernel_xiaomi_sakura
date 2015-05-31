CAF-staging/msm8916_kernel

usb: gadget: Resolve recursive spinlock during remote wakeup
When a USB function wishes to send new data during USB suspend state, it
needs to issue USB remote wakeup and send a function wakeup notification
after then. This scenario leads to recursive spin locking inside the
_usb_func_wakeup() function, because this function gets called recursively.
This function issues remote wakeup, which internally calls the resume
interrupt callback, which calls the _usb_func_wakeup() function again.
This issue is resolved by performing the remote wakeup in a deferred work
context, and this splits the recursion loop.

CRs-fixed: 700667
Change-Id: I59c8efde098781587d29f08cd60e4aa3521949d8
Signed-off-by: Danny Segal <dsegal@codeaurora.org>

drivers/usb/dwc3/gadget.c
@@ -1442,29 +1442,10 @@ static int __dwc3_gadget_ep_queue(struct dwc3_ep *dep, struct dwc3_request *req)
 
 static int dwc3_gadget_wakeup(struct usb_gadget *g)
 {
-	int ret = 0;
-
 	struct dwc3_usb_gadget *dwc3_gadget = g->private;
-	struct dwc3 *dwc = dwc3_gadget->dwc;
-	unsigned long		flags;
-	spin_lock_irqsave(&dwc->lock, flags);
 
-	if (atomic_read(&dwc->in_lpm)) {
-		schedule_work(&dwc3_gadget->wakeup_work);
-		pr_debug("Core is in low-power mode. Scheduling wakeup work.\n");
-		ret = -EBUSY;
-	} else {
-		pr_debug("Core is active. Initiating remote wakeup.\n");
-		ret = dwc3_gadget_wakeup_int(dwc);
-		if (ret)
-			pr_err("Remote wakeup failed. ret = %d\n", ret);
-		else
-			pr_debug("Remote wake up succeeded.\n");
-	}
-
-	spin_unlock_irqrestore(&dwc->lock, flags);
-
-	return ret;
+	schedule_work(&dwc3_gadget->wakeup_work);
+	return 0;
 }
 
 static inline enum dwc3_link_state dwc3_get_link_state(struct dwc3 *dwc)
@@ -1795,7 +1776,6 @@ static int dwc3_gadget_wakeup_int(struct dwc3 *dwc)
 	if (!link_recover_only)
 		dwc3_gadget_wakeup_interrupt(dwc);
 out:
-
 	return ret;
 }
 
@@ -1815,6 +1795,11 @@ static int dwc_gadget_func_wakeup(struct usb_gadget *g, int interface_id)
 
 	ret = dwc3_send_gadget_generic_command(dwc,
 		DWC3_DGCMD_XMIT_FUNCTION, interface_id);
+
+	if (ret)
+		pr_err("Function wakeup HW command failed.\n");
+	else
+		pr_debug("Function wakeup HW command succeeded.\n");
 
 	return ret;
 }

RevanthTolety/kernel_xiaomi_sakura

drivers/usb/dwc3/gadget.c
@@ -1385,27 +1385,9 @@ static int __dwc3_gadget_ep_queue(struct dwc3_ep *dep, struct dwc3_request *req)
 static int dwc3_gadget_wakeup(struct usb_gadget *g)
 {
 	struct dwc3		*dwc = gadget_to_dwc(g);
-	unsigned long		flags;
-	int			ret = 0;
 
-	spin_lock_irqsave(&dwc->lock, flags);
-
-	if (atomic_read(&dwc->in_lpm)) {
-		schedule_work(&dwc->wakeup_work);
-		pr_debug("Core is in low-power mode. Scheduling wakeup work.\n");
-		ret = -EBUSY;
-	} else {
-		pr_debug("Core is active. Initiating remote wakeup.\n");
-		ret = dwc3_gadget_wakeup_int(dwc);
-		if (ret)
-			pr_err("Remote wakeup failed. ret = %d\n", ret);
-		else
-			pr_debug("Remote wake up succeeded.\n");
-	}
-
-	spin_unlock_irqrestore(&dwc->lock, flags);
-
-	return ret;
+	schedule_work(&dwc->wakeup_work);
+	return 0;
 }
 
 static inline enum dwc3_link_state dwc3_get_link_state(struct dwc3 *dwc)
@@ -1740,7 +1722,6 @@ static int dwc3_gadget_wakeup_int(struct dwc3 *dwc)
 	if (!link_recover_only)
 		dwc3_gadget_wakeup_interrupt(dwc);
 out:
-
 	return ret;
 }
 
@@ -1760,6 +1741,11 @@ static int dwc_gadget_func_wakeup(struct usb_gadget *g, int interface_id)
 
 	ret = dwc3_send_gadget_generic_command(dwc,
 		DWC3_DGCMD_XMIT_FUNCTION, interface_id);
+
+	if (ret)
+		pr_err("Function wakeup HW command failed.\n");
+	else
+		pr_debug("Function wakeup HW command succeeded.\n");
 
 	return ret;
 }