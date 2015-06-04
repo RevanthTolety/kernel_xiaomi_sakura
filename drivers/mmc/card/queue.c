MotorolaMobilityLLC/kernel-msm

mmc: queue: add timeout capability to requests
Individual requests can have timeouts defined. The default
timeout is set to 120 seconds. On a timeout, the block layer
notifies the driver and error can be handled thereafter.

Change-Id: Ifcd690411770ab266c12a01bb0993f92b0f18bc6
Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>

drivers/mmc/card/queue.c
@@ -624,6 +624,16 @@ static void mmc_cmdq_error_work(struct work_struct *work)
 	mq->cmdq_error_fn(mq);
 }
 
+enum blk_eh_timer_return mmc_cmdq_rq_timed_out(struct request *req)
+{
+	struct mmc_queue *mq = req->q->queuedata;
+
+	pr_err("%s: request with tag: %d flags: 0x%x timed out\n",
+	       mmc_hostname(mq->card->host), req->tag, req->cmd_flags);
+
+	return mq->cmdq_req_timed_out(req);
+}
+
 int mmc_cmdq_init(struct mmc_queue *mq, struct mmc_card *card)
 {
 	int i, ret = 0;
@@ -666,6 +676,8 @@ int mmc_cmdq_init(struct mmc_queue *mq, struct mmc_card *card)
 	blk_queue_softirq_done(mq->queue, mmc_cmdq_softirq_done);
 	INIT_WORK(&mq->cmdq_err_work, mmc_cmdq_error_work);
 
+	blk_queue_rq_timed_out(mq->queue, mmc_cmdq_rq_timed_out);
+	blk_queue_rq_timeout(mq->queue, 120 * HZ);
 	card->cmdq_init = true;
 
 	goto out;

RevanthTolety/kernel_xiaomi_sakura

Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

drivers/mmc/card/queue.c
@@ -571,6 +571,16 @@ static void mmc_cmdq_error_work(struct work_struct *work)
 	mq->cmdq_error_fn(mq);
 }
 
+enum blk_eh_timer_return mmc_cmdq_rq_timed_out(struct request *req)
+{
+	struct mmc_queue *mq = req->q->queuedata;
+
+	pr_err("%s: request with tag: %d flags: 0x%llx timed out\n",
+	       mmc_hostname(mq->card->host), req->tag, req->cmd_flags);
+
+	return mq->cmdq_req_timed_out(req);
+}
+
 int mmc_cmdq_init(struct mmc_queue *mq, struct mmc_card *card)
 {
 	int i, ret = 0;
@@ -613,6 +623,8 @@ int mmc_cmdq_init(struct mmc_queue *mq, struct mmc_card *card)
 	blk_queue_softirq_done(mq->queue, mmc_cmdq_softirq_done);
 	INIT_WORK(&mq->cmdq_err_work, mmc_cmdq_error_work);
 
+	blk_queue_rq_timed_out(mq->queue, mmc_cmdq_rq_timed_out);
+	blk_queue_rq_timeout(mq->queue, 120 * HZ);
 	card->cmdq_init = true;
 
 	goto out;