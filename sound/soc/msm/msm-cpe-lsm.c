MotorolaMobilityLLC/kernel-msm

ASoC: msm-cpe-lsm: Add 24 bit support for buffering path
Add 24 bit support for TX path bufferring on the CPE path. Adds support
for only SNDRV_PCM_FORMAT_S24_LE format, which is treated as 24 bit data
channel.

Change-Id: I31ac3db8758bdd54968ae5c8b030121dbdde75cb
Signed-off-by: Bhalchandra Gajare <gajare@codeaurora.org>

sound/soc/msm/msm-cpe-lsm.c
@@ -68,7 +68,8 @@ static struct snd_pcm_hardware msm_pcm_hardware_listen = {
 		 SNDRV_PCM_INFO_INTERLEAVED |
 		 SNDRV_PCM_INFO_PAUSE |
 		 SNDRV_PCM_INFO_RESUME),
-	.formats = (SNDRV_PCM_FMTBIT_S16_LE),
+	.formats = (SNDRV_PCM_FMTBIT_S16_LE |
+		    SNDRV_PCM_FMTBIT_S24_LE),
 	.rates = SNDRV_PCM_RATE_16000,
 	.rate_min = 16000,
 	.rate_max = 16000,
@@ -1676,18 +1677,25 @@ static int msm_cpe_lsm_hwparams(struct snd_pcm_substream *substream,
 				/ params_periods(params));
 	lab_hw_params->period_count = params_periods(params);
 	lab_hw_params->sample_rate = params_rate(params);
+
 	if (params_format(params) == SNDRV_PCM_FORMAT_S16_LE)
 		lab_hw_params->sample_size = 16;
+	else if (params_format(params) ==
+		 SNDRV_PCM_FORMAT_S24_LE)
+		lab_hw_params->sample_size = 24;
 	else {
 		pr_err("%s: Invalid Format\n", __func__);
 		return -EINVAL;
 	}
-	pr_debug("%s: Format %d buffer size(bytes) %d period count %d\n"
-		 " Channel %d period in bytes 0x%x Period Size 0x%x\n",
-		 __func__, params_format(params), params_buffer_bytes(params),
-		 params_periods(params), params_channels(params),
-		 params_period_bytes(params), params_period_size(params));
-return 0;
+
+	dev_dbg(rtd->dev,
+		"%s: Format %d buffer size(bytes) %d period count %d\n"
+		" Channel %d period in bytes 0x%x Period Size 0x%x\n",
+		__func__, params_format(params), params_buffer_bytes(params),
+		params_periods(params), params_channels(params),
+		params_period_bytes(params), params_period_size(params));
+
+	return 0;
 }
 
 static snd_pcm_uframes_t msm_cpe_lsm_pointer(

RevanthTolety/kernel_xiaomi_sakura

sound/soc/msm/msm-cpe-lsm.c
@@ -69,7 +69,8 @@ static struct snd_pcm_hardware msm_pcm_hardware_listen = {
 		 SNDRV_PCM_INFO_INTERLEAVED |
 		 SNDRV_PCM_INFO_PAUSE |
 		 SNDRV_PCM_INFO_RESUME),
-	.formats = (SNDRV_PCM_FMTBIT_S16_LE),
+	.formats = (SNDRV_PCM_FMTBIT_S16_LE |
+		    SNDRV_PCM_FMTBIT_S24_LE),
 	.rates = SNDRV_PCM_RATE_16000,
 	.rate_min = 16000,
 	.rate_max = 16000,
@@ -1961,6 +1962,9 @@ static int msm_cpe_lsm_hwparams(struct snd_pcm_substream *substream,
 	hw_params->sample_rate = params_rate(params);
 	if (params_format(params) == SNDRV_PCM_FORMAT_S16_LE)
 		hw_params->sample_size = 16;
+	else if (params_format(params) ==
+		 SNDRV_PCM_FORMAT_S24_LE)
+		hw_params->sample_size = 24;
 	else {
 		dev_err(rtd->dev,
 			"%s: Invalid Format 0x%x\n",
@@ -1974,6 +1978,7 @@ static int msm_cpe_lsm_hwparams(struct snd_pcm_substream *substream,
 		__func__, params_format(params), params_buffer_bytes(params),
 		params_periods(params), params_channels(params),
 		params_period_bytes(params), params_period_size(params));
+
 	return 0;
 }
 