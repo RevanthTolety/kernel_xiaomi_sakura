MotorolaMobilityLLC/kernel-msm

iommu/io-pgtable-arm: Stricter double-map checking
Currently there's a check to make sure that a mapping isn't being
installed on top of an existing leaf mapping.  However, mapping on top
of an existing table mapping is silently ignored.  Make the check more
strict by testing for any valid page table entry (table, block, or page
descriptor) and by making any overlap fatal.

Change-Id: Ibdffbfdb2b44cf9da5044c2c1828a32979a39760
Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

drivers/iommu/io-pgtable-arm.c
@@ -282,8 +282,8 @@ static int arm_lpae_init_pte(struct arm_lpae_io_pgtable *data,
 	arm_lpae_iopte pte = prot;
 
 	/* We require an unmap first */
-	if (iopte_leaf(*ptep, lvl)) {
-		WARN_ON(!suppress_map_failures);
+	if (*ptep & ARM_LPAE_PTE_VALID) {
+		BUG_ON(!suppress_map_failures);
 		return -EEXIST;
 	}
 

RevanthTolety/kernel_xiaomi_sakura

Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

drivers/iommu/io-pgtable-arm.c
@@ -340,8 +340,8 @@ static int arm_lpae_init_pte(struct arm_lpae_io_pgtable *data,
 	struct io_pgtable_cfg *cfg = &data->iop.cfg;
 
 	/* We require an unmap first */
-	if (iopte_leaf(*ptep, lvl)) {
-		WARN_ON(!selftest_running);
+	if (*ptep & ARM_LPAE_PTE_VALID) {
+		BUG_ON(!selftest_running);
 		return -EEXIST;
 	}
 