sbourdelin/linux

f2fs: keep lazytime on remount
This patch fixes losing lazytime when remounting f2fs.

Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

fs/f2fs/super.c
@@ -1550,6 +1550,7 @@ skip:
 		(test_opt(sbi, POSIX_ACL) ? SB_POSIXACL : 0);
 
 	limit_reserve_root(sbi);
+	*flags = (*flags & ~SB_LAZYTIME) | (sb->s_flags & SB_LAZYTIME);
 	return 0;
 restore_gc:
 	if (need_restart_gc) {

RevanthTolety/kernel_xiaomi_sakura

Reviewed-by: Chao Yu <yuchao0@huawei.com>

fs/f2fs/super.c
@@ -1550,6 +1550,7 @@ skip:
 		(test_opt(sbi, POSIX_ACL) ? MS_POSIXACL : 0);
 
 	limit_reserve_root(sbi);
+	*flags = (*flags & ~MS_LAZYTIME) | (sb->s_flags & MS_LAZYTIME);
 	return 0;
 restore_gc:
 	if (need_restart_gc) {